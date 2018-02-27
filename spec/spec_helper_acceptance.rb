78ef32b1218ad7a5ee9870df98396c7fe23a93f2
Erick Banks <erick@puppetlabs.com>
2018-02-26 16:57:16 -0800

    (WIN-69) Move tests to acceptance root add spec_helper_acceptance

---
+++ spec/spec_helper_acceptance.rb
@@ -0,0 +1,12 @@
+require 'beaker-rspec'
+require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
+
+run_puppet_install_helper
+
+install_ca_certs
+
+proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+hosts.each do |host|
+  install_dev_puppet_module_on(host, :source => proj_root, :module_name => 'dsc')
+end
